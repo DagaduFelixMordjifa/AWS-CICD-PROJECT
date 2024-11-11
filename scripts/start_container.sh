4e59c4767f84c15267ae9aae531b708ad890fb48
DagaduFelixMordjifa <dagadufelix@gmail.com>
2024-11-11 16:01:05 +0000

    error check

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull gyenoch/hotel-app:latest
+docker pull dagadufelix/hotel-app:latest
 
 # Run the Docker image as a container
-docker run -dit -p 80:80 gyenoch/hotel-app
+docker run -dit -p 80:80 dagadufelix/hotel-app:latest
